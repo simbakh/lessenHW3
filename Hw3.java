ebf82000420a48ad4496882a3e732cb107369884
simbakh <37250935+simbakh@users.noreply.github.com>
2018-11-28 18:08:13 +0200

    Update Hw3.java

--- Hw3.java
+++ Hw3.java
@@ -7,12 +7,9 @@ public class Hw3 {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the year");
 		int a = sc.nextInt();
-		if (a%4 == 0  && a%400==0)
-		{
+		if (a%4 == 0  && a%400==0){
 			System.out.println("It's a leap-year");
-		}
-		else 
-		{
+		}else {
 			System.out.println("It's a non-leap year");
 		}
 
